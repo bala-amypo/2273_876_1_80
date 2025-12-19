db6cb5e1a3bfa316b96db51b3b059633b9189a9f
bala-amypo <bala@amypo.in>
2025-12-19 07:57:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/EventMergeRecordRepository.java
+++ src/main/java/com/example/demo/repository/EventMergeRecordRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.EventMergeRecord;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 public interface EventMergeRecordRepository extends JpaRepository<EventMergeRecord, Long>{
 }
