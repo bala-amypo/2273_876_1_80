e86e487c4e02c7837e219219390d538858dcf64f
bala-amypo <bala@amypo.in>
2025-12-19 07:54:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/AcademicEventRepository.java
+++ src/main/java/com/example/demo/repository/AcademicEventRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.AcademiProfile;
 
-public interface AcedemicEventRepository extends JpaRepository<AcedemicProfil,Long>{
+public interface AcedemicEventRepository extends JpaRepository<AcedemicEvent,Long>{
 
     
 }
