8dcb67189027f694dcf7c469bc406ef9e743439a
bala-amypo <bala@amypo.in>
2025-12-19 08:27:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/AcademicEventController.java
+++ src/main/java/com/example/demo/controller/AcademicEventController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 @RestController
 public class AcademiEventController{
     
--- src/main/java/com/example/demo/controller/EventMergeRecordController.java
+++ src/main/java/com/example/demo/controller/EventMergeRecordController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
 public class EventMergeRecordController{
     
 }
