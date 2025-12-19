db9b9ced59aa3afcebddb3ab8d6f71623b583901
bala-amypo <bala@amypo.in>
2025-12-19 07:42:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/AcademicEventRepository.java
+++ src/main/java/com/example/demo/repository/AcademicEventRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.AcademiProfile;
+
 public interface AcedemicEventRepository extends JpaRepository<AcedemicProfil,Long>{
-Optional<UserAccount> findByEmail(String email);
+
     
 }
--- src/main/java/com/example/demo/repository/ClashRecordRepository.java
+++ src/main/java/com/example/demo/repository/ClashRecordRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.
 public interface ClashRecordRepository extends JpaRepository<ClashRecord, Long>{
    
 }
