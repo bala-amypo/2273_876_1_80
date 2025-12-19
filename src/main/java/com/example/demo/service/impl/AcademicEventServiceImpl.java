37695b7bfb43c2190e49f16fa7b005499a174df5
bala-amypo <bala@amypo.in>
2025-12-19 06:35:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/AcademicEventServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.demo.service.implement;
+
+public class AcademicEventServiceImpl implements AcademicEventService {
+
+}
---
+++ src/main/java/com/example/demo/service/impl/EventMergeServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.demo.service.implement;
+public class EventMergeServiceImpl implements EventMergeService {
+
+}
