6b3709f63d01ab8ffab4873dc67e1759a0ba9b3f
bala-amypo <bala@amypo.in>
2025-12-21 15:30:43 +0000

    User commit

--- src/main/java/com/example/demo/service/HarmonizedCalendarService.java
+++ src/main/java/com/example/demo/service/HarmonizedCalendarService.java
@@ -1,5 +1,18 @@
 package com.example.demo.service;
 
-public interface HarmonizedCalendarService{
+import com.example.demo.entity.HarmonizedCalendar;
 
+import java.time.LocalDate;
+import java.util.List;
+
+public interface HarmonizedCalendarService {
+
+    HarmonizedCalendar generateHarmonizedCalendar(String title, String generatedBy);
+
+    HarmonizedCalendar getCalendarById(Long id);
+
+    List<HarmonizedCalendar> getAllCalendars();
+
+    List<HarmonizedCalendar> getCalendarsWithinRange(LocalDate start, LocalDate end);
 }
+
