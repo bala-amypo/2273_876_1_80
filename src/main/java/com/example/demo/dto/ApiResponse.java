b9f1d318e37e659343b8cc0b7b1c30c37455a492
bala-amypo <bala@amypo.in>
2025-12-26 15:35:42 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -5,11 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse {
-
     private boolean success;
     private String message;
     private Object data;
 }
+
