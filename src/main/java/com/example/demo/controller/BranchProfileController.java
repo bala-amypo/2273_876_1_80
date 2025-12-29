ece64ffa6a1bd9c871c6f0e3a14ac18cbe1af352
bala-amypo <bala@amypo.in>
2025-12-29 08:18:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/BranchProfileController.java
+++ src/main/java/com/example/demo/controller/BranchProfileController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/branches")
-public class BranchProfileController {
+public class BranchProfileController{
 
     private final BranchProfileService branchProfileService;
 
